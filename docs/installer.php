4b2af818e41e45f9cb3d4535384125dbd06de49a
Sorokovsky <Sorokovskys@ukr.net>
2024-02-05 23:15:09 +0200

    finish

--- docs/installer.php
+++ docs/installer.php
@@ -55,11 +55,11 @@ namespace {
     LogHandler::init_error_handler();
     class DUPX_Bootstrap
     {
-        const ARCHIVE_FILENAME   = 'snapshot_9b81ed16183509885076_20240205205956_archive.zip';
-        const ARCHIVE_SIZE       = '34363101';
+        const ARCHIVE_FILENAME   = 'snapshot_b627d8ca0ca070271348_20240205211405_archive.zip';
+        const ARCHIVE_SIZE       = '34363097';
         const INSTALLER_DIR_NAME = 'dup-installer';
-        const PACKAGE_HASH       = '9b81ed1-05205956';
-        const SECONDARY_PACKAGE_HASH = 'a837583-05205956';
+        const PACKAGE_HASH       = 'b627d8c-05211405';
+        const SECONDARY_PACKAGE_HASH = '829bb58-05211405';
         const VERSION            = '1.5.8';
         const MINIMUM_PHP_VERSION = '5.6.20';
         const ZIP_MODE_AUTO    = 0;
